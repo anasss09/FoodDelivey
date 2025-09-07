e40c24e6b7a4de14a52a49ab06595e2368bb7882
“Anas <anaskhan2209k@gmail.com>
2025-09-07 17:47:28 +0530

    added cors origins

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", `${process.env.CORS_ORIGINS}`],
     credentials: true
 }))
 app.use(bodyParser.json({ limit: "4kb" }));
